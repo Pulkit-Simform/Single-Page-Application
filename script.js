c2aa0225b6b96a9e2c7d5c3ed801b5a7c25edf39
Pulkit Sharma <pulkit@simformsolutions.com>
2023-02-06 13:25:09 +0530

    Data Loaded Successfully

--- script.js
+++ script.js
@@ -101,7 +101,47 @@ document.getElementById("contactForm")?.addEventListener("click",() => {
 })
 
 const addNavbarToContestData = () => {
-  
+  const n = Math.ceil(ALL_SITES_CONTEST.size/3);
+  const addNavArr = [];
+  const navStr = (i) => {
+    return `
+      <li>
+        <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" id="nav-${i}">${i}</a>
+      </li>
+    `
+  }
+
+  for(let i=1;i<=n;i++){
+    addNavArr.push(navStr(i));
+  }
+
+  console.log(...addNavArr)
+
+  const htmlStr = [`
+<nav aria-label="Page navigation example">
+<div class="justify-center items-center m-5 ml-5">
+  <ul class="inline-flex items-center -space-x-px">
+    <li>
+      <a href="#" class="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        <span class="sr-only">Previous</span>
+        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+      </a>
+    </li>`,
+...addNavArr,
+`
+    <li>
+      <a href="#" class="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        <span class="sr-only">Next</span>
+        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+      </a>
+    </li>
+  </ul>
+</nav>
+</div>
+  `]
+
+  document.getElementById("contestData").insertAdjacentHTML("afterend",htmlStr.join(' '))
+
 }
 
 // Data fetch and insert will be occured from here
@@ -114,10 +154,13 @@ window.onload = async () => {
     });
 
     countIn24Hours(data);
-
+    
     ALL_SITES_CONTEST.forEach((i,key) => {
+        // ++c;
+        // if(c > 3) { null; }
         contestDataAdding(i,key);
     });
+      // filter(())
 
     addNavbarToContestData();
   
